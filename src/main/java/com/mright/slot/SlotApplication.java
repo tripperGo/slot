090efff97529877a9b4a64e655b5706aef974e7a
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2020-04-14 13:55:19 +0800

    项目初始化

---
+++ src/main/java/com/mright/slot/SlotApplication.java
@@ -0,0 +1,13 @@
+package com.mright.slot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlotApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mright/slot/jvm/chapter01/HeapOOM.java
@@ -0,0 +1,13 @@
+package com.mright.slot.jvm.chapter01;
+
+/**
+ * @author : zhaochuanzhen
+ * @description :
+ * @date : 2020/4/14 13:53
+ */
+public class HeapOOM {
+
+    public static void main(String[] args) {
+        System.out.println(22);
+    }
+}
